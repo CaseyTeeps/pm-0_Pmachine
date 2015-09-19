520e68eb38652122767e27fb9cd49e52ee324380
Casey Barth <caseybarth@Caseys-MacBook-Pro.local>
2015-09-19 16:04:52 -0400

    Declaration of initial variables and framework

--- pmachine/main.c
+++ pmachine/main.c
@@ -2,14 +2,67 @@
 //  main.c
 //  pmachine
 //
-//  Created by Casey Barth on 9/19/15.
+//  Created by Casey Barth on 9/16/15.
 //  Copyright (c) 2015 Casey Barth. All rights reserved.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+//defined in spec
+#define MAX_STACK_HEIGHT = 2000;
+#define MAX_CODE_LENGTH = 500;
+#define MAX_LEXI_LEVELS = 3
+
+//Prototypes
+void fetch();
+void execute();
+void freeAll(FILE* input);
+
+
+
+int     pc = 0;         //program counter
+int     bp = 1;         //base pointer (point to base of current activation record)
+int     sp = 0;         //stack pointer (points to the top of the stack)
+int     halt = 0;       //when halt is called set bool flag.
+int     arCnt = 0;      //count of activation records
+
+//DECLARE OP CODE ARRAY OF TYPE STRING FOR OUTPUT
+char*  opCode[] = {
+    "",
+    "LIT",
+    "OPR",
+    "LOD",
+    "STO",
+    "CAL",
+    "INC",
+    "JMP",
+    "JPC",
+    "SIO",
+    "SIO",
+    "SIO"
+};
+
+//STRUCT DEFINED FOR INSTRUCTION FORMAT
+struct instruction {
+    int op;             //OP CODE
+    int l;              //LEXIGRAPHICAL LEVEL
+    int m;              //PARAMETER
+};
+
+struct instruction **code;
+struct instruction *IR;
+
+//DECLARE OUTPUT POINTER
+FILE* output;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    //OPEN THE MACHINE CODE
+    FILE* input = fopen("mcode.txt", "r");
+    
+    
+    
+    
     return 0;
 }
